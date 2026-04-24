86d1d8e64974ea216bc5cfbe8ba3bcaf1e6f7895
anant dubey <anant.dubey@bijnis.com>
2026-04-24 20:29:23 +0530

    npm init and tried lodash

--- app.js
+++ app.js
@@ -1,18 +1,4 @@
-import http from 'http';
+const _ = require('lodash')
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Welcome to the Home Page');
-  } else if (req.url === '/about') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('This is the About Page');
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('404: Page Not Found');
-  }
-});
-
-server.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+items = [1, [2,3]]
+console.log(_.flattenDeep(items))
---
+++ http-server.js
@@ -0,0 +1,18 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Welcome to the Home Page');
+  } else if (req.url === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('This is the About Page');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404: Page Not Found');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
