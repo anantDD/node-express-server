ece136ab05c64b0664e5ffc39390fb1778a2ea2d
anant dubey <anant.dubey@bijnis.com>
2026-04-24 14:17:45 +0530

    modules

---
+++ modules.js
@@ -0,0 +1,33 @@
+console.log('hi')
+
+/*
+GLOBALS
+__dirname - path to curr dir
+__filename - file name
+require - function to use modules
+module - info about current module (file). tells you about the exports etc
+process - info about env where program is being executed
+*/
+
+/*
+MODULES
+commonjs - every file is module bby default
+
+*/
+console.log(__dirname)
+
+const names = require('./names')
+const sayHi = require('./utils')
+const data = require('./alternative-exports')
+
+// setInterval(() => {
+//     console.log('hello world');
+// }, 1000)
+console.log('hi2')
+console.log(data)
+
+
+
+sayHi(names.john)
+sayHi(names.peter)
+console.log(names.check)
