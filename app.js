dddfe5aea918b62ec591aaafe050d7716bd9fa44
anant dubey <anant.dubey@bijnis.com>
2026-04-25 17:39:53 +0530

    streams

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-const _ = require('lodash')
+var http = require('http')
+var fs = require('fs')
 
-items = [1, [2,3]]
-console.log(_.flattenDeep(items))
-console.log('hi - testing nodemons')
-console.log('hi - testing nodemons')
+for (let i =0; i <10000; i++){
+  fs.writeFileSync('./content/big.txt', `abc ${i}`, {flag: 'a'})
+}
+
+const server = http.createServer((req, res) => {
+  
+  const fileStream = fs.createReadStream('./content/big.txt', 'utf-8')
+  fileStream.on('open', () => {
+    fileStream.pipe(res)
+  })
+  fileStream.on('error', (err) => {
+    console.log('error')
+    res.end(err)
+  })
+
+})
+
+server.listen(3000, () => {
+  console.log('server is running')
+})
---
+++ nodemon.js
@@ -0,0 +1,6 @@
+const _ = require('lodash')
+
+items = [1, [2,3]]
+console.log(_.flattenDeep(items))
+console.log('hi - testing nodemons')
+console.log('hi - testing nodemons')
